50a18dbf32dba7168a5c1c3424162d4967e20e37
Vojtěch Charouz <21IA08_CHAROUZ@spsjc.cz>
2023-03-27 11:44:18 +0200

    test

--- Projekt/MainWindow.xaml.cs
+++ Projekt/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,8 +11,10 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
 
 namespace Projekt
 {
@@ -20,9 +23,52 @@ namespace Projekt
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public double Y = 0;
+        public bool blockedY = false;
         public MainWindow()
         {
             InitializeComponent();
+            
+            this.KeyDown += (s, e) =>
+            {
+                switch(e.Key)
+                {
+                    case (Key.Space):
+                        if(blockedY==false)
+                        {
+                            blockedY = true;
+
+
+                                Y = Canvas.GetBottom(player);
+
+                                player.SetValue(Canvas.BottomProperty, Y);
+                                
+                            
+
+
+                        }
+
+                        
+                       
+                        break;
+                }
+            };
+
+
+            this.KeyUp += (s, e) =>
+            {
+                switch(e.Key)
+                {
+                    case (Key.Space):
+                        blockedY = false;
+                        break;
+                        
+                }
+            };
+
+
+
         }
     }
 }
