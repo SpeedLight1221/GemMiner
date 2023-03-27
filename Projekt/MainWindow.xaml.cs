f1cf7693362897981a55709719196e8a982aa0ea
SpeedLight1221 <vojtacharouz@gmail.com>
2023-03-27 15:45:59 +0200

    movement right and up

--- Projekt/MainWindow.xaml.cs
+++ Projekt/MainWindow.xaml.cs
@@ -26,59 +26,151 @@ namespace Projekt
     
     public partial class MainWindow : Window
     {
+
+        public bool D;
+        public int Span = 9999;
+
         DispatcherTimer timer = new DispatcherTimer();
 
         double maxHeight = 0.0;
-        public double Y = 0;
+       
         public bool blockedY = false;
         public MainWindow()
         {
             InitializeComponent();
-            timer.Interval = new TimeSpan(70);
-            ;
+            timer.Interval = new TimeSpan(Span);
 
+
+
+
+#region down
             this.KeyDown += (s, e) =>
             {
-                switch(e.Key)
+                if (blockedY == false)
                 {
-                    case (Key.Space):
-
-                        maxHeight = Canvas.GetBottom(player)+70d;
-                        //MessageBox.Show(maxHeight.ToString());
-                   
-
-
-                        break;
-                }
-                timer.Tick += (s, e) =>
-                {
-                    double y = Canvas.GetBottom(player);
-                    Canvas.SetBottom(player, y + 1.0d);
-                    if (y + 1.0 >= maxHeight)
+                    switch (e.Key)
                     {
-                        timer.Stop();
+                        
+
+
+                        case (Key.Space):
+
+                            
+                            blockedY = true;
+
+                            jump();
+                            break;
+
+
+
+
+                        case (Key.D):
+                            D = true;
+                            break;
+
+
+
+                        
                     }
-                };
+                    
 
 
 
-                timer.Start();
+                    
+                }
             };
-
-
+#endregion
+#region Up
             this.KeyUp += (s, e) =>
             {
                 switch(e.Key)
                 {
                     case (Key.Space):
-                        blockedY = false;
+                        
+                        
                         break;
+
+                    
                         
                 }
+
+
             };
+#endregion
 
 
 
+
+
+
+        }
+
+
+        public void jump()
+        {
+            maxHeight = Canvas.GetBottom(player) + 150d;
+            Canvas.SetBottom(player, maxHeight - 69.5d);
+            //MessageBox.Show(maxHeight.ToString());
+            timer.Tick += (s, e) =>
+            {
+                double y = Canvas.GetBottom(player);
+                Canvas.SetBottom(player, y + 0.5d);
+                if (D) { Canvas.SetLeft(player,Canvas.GetLeft(player)+0.2d); }
+
+                if (y + 0.5 >= maxHeight)
+                {
+                    timer.Stop();
+                    Cooldown();
+                    Gravity();
+                }
+            };
+            timer.Start();
+        }
+        public void Gravity()
+        {
+            
+            double y = Canvas.GetBottom(player);
+           
+            DispatcherTimer gravTimer = new DispatcherTimer();
+            gravTimer.Interval = new TimeSpan(Span);
+            gravTimer.Tick += (s, e) =>
+            {
+                if (y > 0)
+                {
+                    Canvas.SetBottom(player, y - 0.5d);
+                    y = Canvas.GetBottom(player);
+                }
+                else if (y < 0)
+                {
+                    Canvas.SetBottom(player, 0);
+                    gravTimer.Stop();
+                }
+                else
+                {
+                    gravTimer.Stop();
+                }
+            };
+            gravTimer.Start();
+        }
+
+
+        public void Cooldown()
+        {
+            DispatcherTimer cdTimer = new DispatcherTimer();
+            player.Fill = Brushes.Red;
+            cdTimer.Interval = new TimeSpan(0,0,0,0,60);
+            cdTimer.Tick += (s, e) =>
+            {
+                if (Canvas.GetBottom(player) == 0)
+                {
+                    blockedY = false;
+                    cdTimer.Stop();
+                    player.Fill = Brushes.Blue;
+                }
+            };
+            cdTimer.Start();
+            
+            
         }
     }
 }
