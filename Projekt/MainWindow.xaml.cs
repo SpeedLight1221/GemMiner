13f8658b968a040d9fdffa246cb42517614cb356
SpeedLight1221 <vojtacharouz@gmail.com>
2023-03-27 22:01:36 +0200

    Movement Up

--- Projekt/MainWindow.xaml.cs
+++ Projekt/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
@@ -31,12 +33,14 @@ namespace Projekt
         public bool D = false;
         public bool A = false;
         public bool CD = false;
-        public int Span = 10000;
+        public int Span = 200;
         public double Size = 100d;
         public double level = 0;
+        public bool moving =false;
 
         DispatcherTimer Movetimer = new DispatcherTimer();
-
+        //create storyboard
+        Storyboard move = new Storyboard();
         double maxHeight = 0.0;
         double maxRight = 0.0;
 
@@ -44,16 +48,34 @@ namespace Projekt
         public MainWindow()
         {
             InitializeComponent();
-            Movetimer.Interval = new TimeSpan(Span);
 
-            this.KeyDown += (s,e) =>
+
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            this.KeyDown += (s, e) =>
             {
-                if (CD == false)
-                {
+                
                     switch (e.Key)
                     {
                         case Key.Space:
                             Jump();
+                            
                             break;
                         case Key.D:
                             D = true;
@@ -64,7 +86,7 @@ namespace Projekt
                             D = false;
                             break;
                     }
-                }
+                
             };
 
 
@@ -72,81 +94,106 @@ namespace Projekt
 
 
 
-            
+
+
 
         }
+
+        
 
 
         public void Jump()
         {
-            maxHeight = Canvas.GetBottom(player)+101d ;
-            maxRight = Canvas.GetLeft(player)+10d;
-           
-            Movetimer.Tick += (s, e) =>
-            {
-                double Y = 0;
-                double X = 0;
-                if (D == true)//if jumping and moving right
-                {
-                    CoolDown();
-
-
-                    Y = Canvas.GetBottom(player);
-                    X = Canvas.GetLeft(player);
-
-
-                    Canvas.SetBottom(player, Y + 0.5d);
-                    Y = Canvas.GetBottom(player);
-                    Canvas.SetLeft(player, X + 0.2d);
-                    X = Canvas.GetLeft(player);
-                }
-                else if(A ==true)//if jumping and moving left
-                {
-                    
-
-                }
-                else// if only  jumping
-                {
-                    
-                    CoolDown();
-                    
-                    
-                    Y = Canvas.GetBottom(player);
-                    
-
-                    Canvas.SetBottom(player, Y + 0.5d);
-                     Y = Canvas.GetBottom(player);
-                    
-                }
-                if (Y + 0.5d >= maxHeight)
-                {
-                    
-                    Movetimer.Stop();
-                    D = false;
-                    A = false;
-                    //Gravity
-                }
-            };
-            Movetimer.Start();
-        }
-
-
-        public void CoolDown()
-        {
-           
-            CD = true;
+            if (CD || moving) { return; }
+            moving = true;
+            move.Completed -= JumpCompleted;
+            move.Completed += JumpCompleted;
+            Animation(Canvas.GetLeft(player), Canvas.GetLeft(player), Canvas.GetBottom(player), Canvas.GetBottom(player) + 150d,true);
             
-            DispatcherTimer CDTimer = new DispatcherTimer();
-            CDTimer.Interval = new TimeSpan(0,0,1);
-            CDTimer.Tick += (s, e) =>
-            {
-                CD = false;
-                
-                
-                CDTimer.Stop();
-
-            };
-            CDTimer.Start();
         }
+
+        public void JumpCompleted(object sender,EventArgs e)
+        {
+            move.Completed -= JumpCompleted;
+           
+            Gravity();
+        }
+
+
+        public void Gravity()
+        {
+            moving = false;
+
+            if (CD|| moving) { return; }
+            moving = true;
+            move.Completed -= GravityCompleted;
+            move.Completed += GravityCompleted;
+            foreach (var control in MyCan.Children.OfType<Rectangle>()) // checks if we arent on a higher ground
+            {
+                if (control.Tag as string== "CollideY")
+                {
+                    if ((Canvas.GetLeft(control) < Canvas.GetLeft(player) + Size) && (Canvas.GetLeft(control) + Size > Canvas.GetLeft(player)))
+                       {
+                        level = Canvas.GetBottom(control) + 100;
+                        
+                    }
+                }
+            }
+
+            Animation(Canvas.GetLeft(player), Canvas.GetLeft(player), Canvas.GetBottom(player), level, false);
+            
+
+        }
+
+        public void GravityCompleted(object sender, EventArgs e)
+        {
+            moving = false;
+            move.Completed -= GravityCompleted;
+            CD = false;
+            
+
+        }
+        public void Animation(double x1, double x2, double y1, double y2,bool Grav)
+        {
+            
+
+
+            //definy movement X (left-right)
+            DoubleAnimation AnimX = new DoubleAnimation();
+            AnimX.From = x1;
+            AnimX.To = x2;
+            AnimX.Duration = new Duration(TimeSpan.FromSeconds(0.2));
+            //define movement Y (Down-Up)
+            DoubleAnimation AnimY = new DoubleAnimation();
+            AnimY.From = y1;
+            AnimY.To = y2;
+            AnimY.Duration = new Duration(TimeSpan.FromSeconds(0.2));
+
+            //set target x
+            Storyboard.SetTarget(AnimX, player);
+            Storyboard.SetTargetProperty(AnimX, new PropertyPath("(Canvas.Left)"));
+            //set target y
+            Storyboard.SetTarget(AnimY, player);
+            Storyboard.SetTargetProperty(AnimY, new PropertyPath("(Canvas.Bottom)"));
+
+            //adding children
+            move.Children.Add(AnimY);
+            move.Children.Add(AnimX);
+            
+
+          
+               
+
+            
+            move.Completed += (s, e) => { CD = false; moving = false; };
+            move.Begin();
+            
+            
+            
+            
+            
+        }
+
+        
     }
 }
